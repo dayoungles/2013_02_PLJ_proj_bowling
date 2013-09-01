f228439e59548907a56269f107e41b6b473a920f
dayoungles <dayoungles@naver.com>
2013-09-01 22:13:55 +0900

    roll()의 프레임 10번 예외처리 확

--- Bowling/test/RollingTest.java
+++ Bowling/test/RollingTest.java
@@ -1,23 +1,32 @@
 import junit.framework.TestCase;
 
-
 public class RollingTest extends TestCase {
 	private Rolling rolling;
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		rolling = new Rolling();
 		super.setUp();
 	}
+
 	public void testGetPinCount() throws Exception {
-		assertEquals(8, rolling.getPinCount());
+		assertEquals(8, rolling.getPinCount(10));
 	}
 
 	public void testRoll() throws Exception {
-		rolling.roll(1);
-		//assertEquals(8, rolling.frameList.get(0).getPinFromFrame(1).getCollapsedPin());
-		CalculateScore c = new CalculateScore();
-		System.out.println(c.calculate());
+		rolling.roll(0);
+		assertEquals(8, rolling.frameList.get(0).getPin(0).getCollapsedPin());// 입력 숫자에 따라 확인 가능..
 	}
+		
+	public void testFrame10() throws Exception {
+		//테스트 하려면 9번 프레임으로 넣되 앞 인덱스가 없어서 get(0)번으로 테스트..
+		//마지막 프레임 경우의 수 
+		// 숫자 숫자 
+		rolling.roll(9);
+		// 스트라이크-숫자- 스페
+		assertEquals('X', rolling.frameList.get(0).getPin(0).getSymbol());
+		assertEquals('/', rolling.frameList.get(0).getPin(2).getSymbol());
+	}
+
 	
 }
