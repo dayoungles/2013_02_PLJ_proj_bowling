f022e2b74851756679a1dc5871d189ec27421611
dayoungles <dayoungles@naver.com>
2013-09-01 23:06:57 +0900

    test getPins

--- Bowling/test/FrameTest.java
+++ Bowling/test/FrameTest.java
@@ -1,10 +1,21 @@
 import junit.framework.TestCase;
 
 public class FrameTest extends TestCase {
+	private Frame frame;
+	@Override
+	protected void setUp() throws Exception {
+		frame = new Frame(0);
+		super.setUp();
+	}
 
 	public void testPinFromFrame() throws Exception {
-		Frame frame = new Frame(1);
-		frame.addPin2Frame(new Pin(1,1));
+		frame.addPin2Frame(new Pin(1,0));
 		assertEquals(1, frame.getPin(0).getCollapsedPin());
+	}
+	public void testGetPins() throws Exception {
+		frame.addPin2Frame(new Pin(1,0));
+		frame.addPin2Frame(new Pin(7,0));
+		frame.getPins();
+		assertEquals(8, frame.getPins());
 	}
 }
